5152b62e92d342b954096f3960bfa5d7d009e3d8
uday755 <79998578+uday755@users.noreply.github.com>
2024-02-17 02:08:53 +0530

    Started Social Media Dashboard

--- Issue Tracker System/routes/api.js
+++ Issue Tracker System/routes/api.js
@@ -1,6 +1,30 @@
 'use strict';
 
+let mongoDB = require("mongodb");
+let mongoose = require("mongoose");
+
+
+const uri = "mongodb+srv://udaysinghtohana9:shivbaba123@cluster0.vimqh.mongodb.net/CodeAlpha_Tasks?retryWrites=true&w=majority";
+
+
+
 module.exports = function (app) {
+
+  mongoose.connect(uri);
+  let issueSchema = new mongoose.Schema({
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_by : {type: String, required: true},
+    assigned_to : String,
+    status_text : String,
+    open: {type: Boolean, required: true},
+    created_on: {type: Date, required: true},
+    updated_on: {type: Date, required: true},
+    project: String
+  })
+
+  let Issue = mongoose.model('Issue', issueSchema);
+
 
   app.route('/api/issues/:project')
   
@@ -11,6 +35,22 @@ module.exports = function (app) {
     
     .post(function (req, res){
       let project = req.params.project;
+      let newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true,
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        project: project
+      });
+      newIssue.save((error, savedIssue)=>{
+        if(!error && savedIssue){
+          console.log(savedIssue);
+        }
+      })
       
     })
     
--- Issue Tracker System/server.js
+++ Issue Tracker System/server.js
@@ -9,8 +9,32 @@ require('dotenv').config();
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 let app = express();
+
+// const uri = "mongodb+srv://udaysinghtohana9:shivbaba123@cluster0.vimqh.mongodb.net/CodeAlpha_Tasks?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
